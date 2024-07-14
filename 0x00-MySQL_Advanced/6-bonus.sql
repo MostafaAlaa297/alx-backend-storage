4f2fec702e4d976de15e1134eacb4a8982e8cec5
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-14 13:24:17 +0000

    fix: add semicolons to bonus task

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,19 +1,18 @@
---- Stored procedure that adds a new correction for a student
-
-DELIMITER//
+MITER //
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
-	DECLARE project_id INT;
+    DECLARE project_id INT;
 
-	SELECT id INTO project_id FROM projects WHERE name = project_name;	
+    -- Try to find the project_id
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
 
-	IF project_id IS NULL THEN	
+    -- If the project does not exist, insert it and get the new id
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
+    END IF;
 
-		INSERT INTO  projects (name) VALUES (project_name);
-		SET project_id = LAST_INSERT_ID();
-	END IF;
-
-	INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
+    -- Insert the correction
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END//
-
-DELIMITER;
+DELIMITER ;
