1d2223cc46457c8653dcc97660089982951109f8
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-14 16:55:59 +0000

    fix: average_score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,23 +1,24 @@
 -- Stored procedure computes and store the average score for a student
 
-DELIMITER//
+DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE total_score INT DEFAULT 0;
-	DECLARE projects_count INT DEFAULT 0;
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
 
-	SELECT SUM(score)
-		INTO total_score
-		FROM corrections
-		WHERE corrections.user_id = user_id;
-	SELECT COUNT(*)
-		INTO projects_count
-		FROM corrections
-		WHERE corrections.user_id = user_id;
+    -- Calculate the total score for the user
+    SELECT IFNULL(SUM(score), 0) INTO total_score
+    FROM corrections
+    WHERE corrections.user_id = user_id;
 
-	UPDATE users
-		SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
-		WHERE users.id = user_id;
-END//
+    -- Calculate the number of projects for the user
+    SELECT COUNT(*) INTO projects_count
+    FROM corrections
+    WHERE corrections.user_id = user_id;
 
-DELIMITER;
+    -- Update the user's average score
+    UPDATE users
+    SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+    WHERE users.id = user_id;
+END //
+DELIMITER ;
