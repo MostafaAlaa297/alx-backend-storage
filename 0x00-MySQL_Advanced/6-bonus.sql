6c6c2c32ad1004490f92ad8bc262f512d636c76d
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-14 16:34:04 +0000

    fix: bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,4 +1,6 @@
-MITER //
+-- Write a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
+
+DELIMITER //
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
     DECLARE project_id INT;
