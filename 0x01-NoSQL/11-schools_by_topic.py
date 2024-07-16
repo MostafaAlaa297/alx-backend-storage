1435f836063416ab89e6f9c5d6b481dfbde57823
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-16 16:19:37 +0000

    finish find document

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -8,4 +8,4 @@ Pymongo module
 
 def schools_by_topic(mongo_collection, topic):
 	"""changes all topics of a school document based on the name"""
-	return [doc for doc in mongo_collection.find_many({ "topic": topic })]
+	return [doc for doc in mongo_collection.find({ "topics": topic })]
