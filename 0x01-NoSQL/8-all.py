09130ea363793b1736e4678eb278b38796107038
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-16 15:34:44 +0000

    finish all pymongo

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
-
 """
 ==============
 Pymongo module
 ==============
 """
 
-import pymongo
-
 
 def list_all(mongo_collection):
 	"""Return an empty list if no document in the collection"""
-	return mongo_collection
+	return [document for document in mongo_collection.find()]
