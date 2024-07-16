a23feaaf132fe93a191f4d5fc87a52c190582855
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-16 17:57:54 +0000

    finish log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,24 +6,24 @@ logsDB
 """
 from pymongo import MongoClient
 
-
 client = MongoClient('mongodb://127.0.0.1:27017')
 nginx = client.logs.nginx
 
-logs_count = nginx.count_documents()
-get_count = nginx.count_documents({ "method": "GET" })
-post_count = nginx.count_documents({ "method": "POST" })
-put_count = nginx.count_documents({ "method": "PUT" })
-patch_count = nginx.count_documents({ "method": "PATCH" })
-delete_count = nginx.count_documents({ "method": "DELETE" })
-path = nginx.count_documents({ "method": "GET" }, { "path": "/status" })
+logs_count = nginx.count_documents({})
+get_count = nginx.count_documents({"method": "GET"})
+post_count = nginx.count_documents({"method": "POST"})
+put_count = nginx.count_documents({"method": "PUT"})
+patch_count = nginx.count_documents({"method": "PATCH"})
+delete_count = nginx.count_documents({"method": "DELETE"})
+status_check_count = nginx.count_documents({"method": "GET", "path": "/status"})
 
 print("{} logs".format(logs_count))
 print("Methods:")
-print("\t method GET: {}".format(get_count))
-print("\t method POST: {}".format(post_count))
-print("\t method PUT: {}".format(put_count))
-print("\t method PATCH: {}".format(patch_count))
-print("\t method DELETE: {}".format(delete_count))
+print("\tmethod GET: {}".format(get_count))
+print("\tmethod POST: {}".format(post_count))
+print("\tmethod PUT: {}".format(put_count))
+print("\tmethod PATCH: {}".format(patch_count))
+print("\tmethod DELETE: {}".format(delete_count))
+print("{} status check".format(status_check_count))
 
-print("{} status check".format(path))
+client.close()
