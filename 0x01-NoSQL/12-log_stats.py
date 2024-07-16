b9c525a9d7a1a375738033f7daf51628c3ad5fc6
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-16 18:10:13 +0000

    finish log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,36 +1,34 @@
 #!/usr/bin/env python3
 """
-======
-logsDB
-======
+nginx stats model
 """
 from pymongo import MongoClient
 
 
-def log_stats(nginx):
-	"""return log stats"""
-	logs_count = nginx.count_documents({})
-	get_count = nginx.count_documents({"method": "GET"})
-	post_count = nginx.count_documents({"method": "POST"})
-	put_count = nginx.count_documents({"method": "PUT"})
-	patch_count = nginx.count_documents({"method": "PATCH"})
-	delete_count = nginx.count_documents({"method": "DELETE"})
-	status_check_count = nginx.count_documents(
-						{
-							"method": "GET", "path": "/status"
-						})
+def log_stats(nginx_collection):
+    """
+    get nginx data
+    """
+    log_count = nginx_collection.count_documents({})
+    get_count = nginx_collection.count_documents({"method": "GET"})
+    post_count = nginx_collection.count_documents({"method": "POST"})
+    put_count = nginx_collection.count_documents({"method": "PUT"})
+    patch_count = nginx_collection.count_documents({"method": "PATCH"})
+    delete_count = nginx_collection.count_documents({"method": "DELETE"})
 
-	print("{} logs".format(logs_count))
-	print("Methods:")
-	print("\tmethod GET: {}".format(get_count))
-	print("\tmethod POST: {}".format(post_count))
-	print("\tmethod PUT: {}".format(put_count))
-	print("\tmethod PATCH: {}".format(patch_count))
-	print("\tmethod DELETE: {}".format(delete_count))
-	print("{} status check".format(status_check_count))
+    path = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+
+    print("{} logs".format(log_count))
+    print("Methods:")
+    print("\tmethod GET: {}".format(get_count))
+    print("\tmethod POST: {}".format(post_count))
+    print("\tmethod PUT: {}".format(put_count))
+    print("\tmethod PATCH: {}".format(patch_count))
+    print("\tmethod DELETE: {}".format(delete_count))
+    print("{} status check".format(path))
 
 if __name__ == "__main__":
-	client = MongoClient('localhost',27017)
-        nginx_collection = client.logs.nginx
-	log_stats(nginx_collection)
+    client = MongoClient('localhost',27017)
+    nginx_collection = client.logs.nginx
 
+    log_stats(nginx_collection)
