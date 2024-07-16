3e7eb26d9caf0c9e952f5feae440d1f3f52271a4
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-16 16:05:45 +0000

    finish update with list

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -8,4 +8,4 @@ Pymongo module
 
 def update_topics(mongo_collection, name, topics):
 	"""changes all topics of a school document based on the name"""
-	mongo_collection.update_one({ "name": name }, { "$push": { "topics": topics } });
+	mongo_collection.update_many({ "name": name }, { "$set": { "topics": topics } });
