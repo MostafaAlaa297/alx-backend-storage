f36d7dab0bdb4b47bd5ca363428898a6d964cdfe
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-16 18:02:18 +0000

    finish log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,24 +6,29 @@ logsDB
 """
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx = client.logs.nginx
 
-logs_count = nginx.count_documents({})
-get_count = nginx.count_documents({"method": "GET"})
-post_count = nginx.count_documents({"method": "POST"})
-put_count = nginx.count_documents({"method": "PUT"})
-patch_count = nginx.count_documents({"method": "PATCH"})
-delete_count = nginx.count_documents({"method": "DELETE"})
-status_check_count = nginx.count_documents({"method": "GET", "path": "/status"})
+def log_stats(nginx):
+	"""return log stats"""
 
-print("{} logs".format(logs_count))
-print("Methods:")
-print("\tmethod GET: {}".format(get_count))
-print("\tmethod POST: {}".format(post_count))
-print("\tmethod PUT: {}".format(put_count))
-print("\tmethod PATCH: {}".format(patch_count))
-print("\tmethod DELETE: {}".format(delete_count))
-print("{} status check".format(status_check_count))
+	logs_count = nginx.count_documents({})
+	get_count = nginx.count_documents({"method": "GET"})
+	post_count = nginx.count_documents({"method": "POST"})
+	put_count = nginx.count_documents({"method": "PUT"})
+	patch_count = nginx.count_documents({"method": "PATCH"})
+	delete_count = nginx.count_documents({"method": "DELETE"})
+	status_check_count = nginx.count_documents({"method": "GET", "path": "/status"})
 
-client.close()
+	print("{} logs".format(logs_count))
+	print("Methods:")
+	print("\tmethod GET: {}".format(get_count))
+	print("\tmethod POST: {}".format(post_count))
+	print("\tmethod PUT: {}".format(put_count))
+	print("\tmethod PATCH: {}".format(patch_count))
+	print("\tmethod DELETE: {}".format(delete_count))
+	print("{} status check".format(status_check_count))
+
+if __name__ == "__main__":
+	client = MongoClient('mongodb://127.0.0.1:27017')
+        nginx = client.logs.nginx
+	log_stats(nginx)
+
