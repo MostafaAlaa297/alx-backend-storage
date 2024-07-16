6015301a1db7e4cb302115f3c2fd4fd0a5820436
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-16 15:26:11 +0000

    finish all pymongo

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+==============
+Pymongo module
+==============
+"""
+
+import pymongo
+
+def list_all(mongo_collection):
+	"""Return an empty list if no document in the collection"""
+	return mongo_collection
