fded0253ccff39c25faaa3d37e876c265676e06c
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-16 15:55:19 +0000

    finish insert school

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -9,4 +9,4 @@ Pymongo module
 def insert_school(mongo_collection, **kwargs):
 	"""Return an empty list if no document in the collection"""
 	result = mongo_collection.insert_one(kwargs)
-	return result.insertd_id
+	return result.inserted_id
