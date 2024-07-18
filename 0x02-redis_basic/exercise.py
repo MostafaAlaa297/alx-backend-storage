1a051acf116ed68b1e151c13a0cd87186801fc33
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-18 13:11:41 +0000

    fix: redis excercise 0/4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,17 +4,18 @@ Redis Python
 """
 import redis
 import uuid
-r = redis.Redis(host='localhost', port=6379, db=0)
+from typing import Union
 
 
 class Cache:
 	"""Cache class"""
 	def __init__(r):
 		"""Initialization method"""
-		self._redis = r
-		r.flushdb()
-	def store(data: str | int | bytes | float) -> str:
+		self._redis = redis.Redis(host='localhost', port=6379, db=0)
+        	self._redis.flushdb()
+
+	def store(self, data: Union[str, int, bytes, float]) -> str:
 		"""Method that returns a uuid string"""
 		key = str(uuid.uuid4())
-		r.set(key, data)
+		self._redis.set(key, data)
 		return key
