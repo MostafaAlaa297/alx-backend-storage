74c0af8b3278337b5700a37ab50e9b2bbeff6f66
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-19 12:32:57 +0000

    finish excercise 2/4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,15 @@ import uuid
 from typing import Union, Callable, Optional
 
 
+def count_calls(method: Callable) -> Callable:
+	"""Decorator to count calls to a method"""
+	@functools.wraps(method)
+	def wrapper(self, *args, **kwargs):
+		"""Wrapper function to increment the call count"""
+		self._redis.incr(method.__qualname__)
+		return method(self, *args, **kwargs)
+	return wrapper
+
 class Cache:
 	"""Cache class"""
 	def __init__(r):
@@ -19,7 +28,7 @@ class Cache:
         	key = str(uuid.uuid4())
         	self._redis.set(key, data)
         	return key
-
+	
 	def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, None]:
 		"""used to convert key data back to the desired format"""
 		value = self._redis.get(key)
@@ -32,8 +41,10 @@ class Cache:
 
 	def get_str(self, key: str) -> Optional[str]:
         	"""Retrieve a string from Redis"""
-		return self.get(key, lambda d: d.decode(key))
+		return Cache.get(key, lambda d: d.decode(key))
 
 	def get_str(self, key: str) -> Optional[str]:
                 """Retrieve an integer from Redis"""
-                return self.get(key, lambda d: int(d))
+                return Cache.get(key, lambda d: int(d))
+
+	return method.__qualname__
