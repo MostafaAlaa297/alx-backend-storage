25ad2ad9cad37ffd9ade8da4bee245a46b838f49
MostafaAlaa297 <elaskary609@gmail.com>
2024-07-19 14:21:36 +0000

    finish excercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -39,7 +39,9 @@ class Cache:
 		"""Initialization method"""
 		self._redis = redis.Redis(host='localhost', port=6379, db=0)
         	self._redis.flushdb()
-	
+
+	@count_calls
+	@call_history	
 	def store(self, data: Union[str, bytes, int, float]) -> str:
         	"""Method that stores data in Redis with a random key and returns the key"""
         	key = str(uuid.uuid4())
